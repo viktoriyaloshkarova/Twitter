7e7b79d06c0dabcd039396cdc886d3ce70ffea45
Viktoriya Loshkarova <viktoriya.loshkarova0419@gmail.com>
2021-03-06 19:20:10 -0500

    Implemented a twitter app

---
+++ Twitter/TweetCellTableViewCell.swift
@@ -0,0 +1,31 @@
+//
+//  TweetCellTableViewCell.swift
+//  Twitter
+//
+//  Created by Viktoriya Loshkarova on 3/6/21.
+//  Copyright © 2021 Dan. All rights reserved.
+//
+
+import UIKit
+
+class TweetCellTableViewCell: UITableViewCell {
+    
+    
+    @IBOutlet weak var profileImageView: UIImageView!
+    
+    @IBOutlet weak var userNameLabel: UILabel!
+    
+    @IBOutlet weak var tweetContent: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
